a28107aca9e983463bbf8fa90e93c2665cae33f8
Hugo Vasconcellos <vasconcelloshugo09@gmail.com>
2022-10-03 14:06:13 -0300

    feat(./*):create project repository

---
+++ api/models/pessoas.js
@@ -0,0 +1,39 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  const Pessoas = sequelize.define('Pessoas', {
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcValidadora: function(dado) {
+          if(dado.length < 3) throw new Error('O campo nome deve ter mais de três caracters!')
+        }
+      }
+    },
+    ativo: DataTypes.BOOLEAN,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: { args: true, msg: 'Dado do tipo e-mail inválido!' }
+      }
+    },
+    role: DataTypes.STRING
+  }, { 
+    paranoid: true,
+    defaultScope: {
+      where: {ativo: true}
+    },
+    scopes: {
+      todos: { where: {} }
+    }
+  })
+  Pessoas.associate = function(models) {
+    Pessoas.hasMany(models.Turmas, {
+      foreignKey: 'docente_id'
+    }) 
+    Pessoas.hasMany(models.Matriculas, {
+      foreignKey: 'estudante_id'
+    })
+
+  }
+  return Pessoas
+}
---
+++ api/routes/pessoasRoute.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const PessoaController = require('../controllers/PessoaController')
+
+const router = Router()
+
+router
+  .get('/pessoas', PessoaController.pegaTodasAsPessoasAtivas)
+  .get('/pessoas/todos', PessoaController.pegaTodasAsPessoas)
+  .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+  .post('/pessoas', PessoaController.criaPessoa)
+  .put('/pessoas/:id', PessoaController.atualizaPessoa)
+  .delete('/pessoas/:id', PessoaController.apagaPessoa)
+  .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
+
+  .get('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.pegaUmaMatricula)
+  .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+  .put('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.atualizaMatricula)
+  .delete('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.apagaMatricula)
+
+module.exports = router
